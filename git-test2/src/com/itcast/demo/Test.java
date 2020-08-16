dc42d88445c31eff6d15b22dddfbf79746dc45c2
nsclj <1733815442@qq.com>
2020-08-16 17:05:48 +0800

    2

--- git-test2/src/com/itcast/demo/Test.java
+++ git-test2/src/com/itcast/demo/Test.java
@@ -3,5 +3,7 @@ package com.itcast.demo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
