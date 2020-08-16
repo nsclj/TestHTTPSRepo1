3b95c3995a365a848e1eae8667b4a76e4cd52b1e
nsclj <1733815442@qq.com>
2020-08-16 16:24:30 +0800

    添加项目

---
+++ git-test/src/com/itcast/Test1.java
@@ -0,0 +1,10 @@
+package com.itcast;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
---
+++ git-test/src/com/itcast/Test2.java
@@ -0,0 +1,12 @@
+package com.itcast;
+
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("hello22");
+        System.out.println("hello22");
+        System.out.println("hello22");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
